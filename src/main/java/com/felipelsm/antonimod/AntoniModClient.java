9afa3a9d482dd235955acddbebbe210f0594ab61
felipelsm <felipelsm2005@gmail.com>
2026-04-22 11:36:54 -0400

    First stable commit

---
+++ src/main/java/com/felipelsm/antonimod/AntoniModClient.java
@@ -0,0 +1,12 @@
+package com.felipelsm.antonimod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AntoniModClient implements ClientModInitializer
+{
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
---
+++ src/main/java/com/felipelsm/antonimod/AntoniModDataGenerator.java
@@ -0,0 +1,13 @@
+package com.felipelsm.antonimod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AntoniModDataGenerator implements DataGeneratorEntrypoint
+{
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator)
+	{
+
+	}
+}
